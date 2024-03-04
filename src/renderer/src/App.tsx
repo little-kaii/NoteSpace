edcdad15e029a4b8d13cf72b2beb21185d1f6dcc
/Akash <akashdebnathwd@gmail.com>
2024-03-04 12:33:20 +0530

    Upto Layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,9 +2,9 @@ import { Content, RootLayout, Sidebar } from '@/components'
 
 function App() {
   return (
-    <RootLayout>
-      <Sidebar className="p-2 border-4 border-red-500">Sidebar</Sidebar>
-      <Content className="border-4 border-blue-500">Content</Content>
+    <RootLayout className="">
+      <Sidebar className="p-2">Sidebar</Sidebar>
+      <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
     </RootLayout>
   )
 }
