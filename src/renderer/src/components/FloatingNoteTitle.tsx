38dfea4ae3bd4cfee7c431e03c8283bfb15c2612
/Akash <akashdebnathwd@gmail.com>
2024-03-05 18:45:52 +0530

    Added jotai to our project and created new components and hook

---
+++ src/renderer/src/components/FloatingNoteTitle.tsx
@@ -0,0 +1,16 @@
+import { selectedNoteAtom } from '@renderer/store'
+import { useAtomValue } from 'jotai'
+import React, { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export const FloatingNoteTitle = ({ className, ...props }: ComponentProps<'div'>) => {
+  const selectedNote = useAtomValue(selectedNoteAtom)
+
+  if (!selectedNote) return null
+
+  return (
+    <div {...props} className={twMerge('flex justify-center', className)}>
+      <span className="text-gray-400 text-sm">{selectedNote.title}</span>
+    </div>
+  )
+}
---
+++ src/renderer/src/hooks/useNotesList.tsx
@@ -0,0 +1,23 @@
+import { notesAtom, selectedNoteIndexAtom } from '@renderer/store'
+import { useAtom, useAtomValue } from 'jotai'
+import React from 'react'
+
+export const useNotesList = ({ onSelect }: { onSelect?: () => void }) => {
+  const notes = useAtomValue(notesAtom)
+
+  const [selectedNoteIndex, setSelectedNoteIndex] = useAtom(selectedNoteIndexAtom)
+
+  const handleNoteSelect = (index: number) => async () => {
+    setSelectedNoteIndex(index)
+
+    if (onSelect) {
+      onSelect()
+    }
+  }
+
+  return {
+    notes,
+    selectedNoteIndex,
+    handleNoteSelect
+  }
+}
