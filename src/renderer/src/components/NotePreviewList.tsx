5fe2b865c2e13df0f3d4fdf7ea7a008a8fb3c2b5
/Akash <akashdebnathwd@gmail.com>
2024-03-05 17:20:41 +0530

    Added MDXEditor

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,10 +1,19 @@
 import { notesMock } from '@/store/mocks'
 import { ComponentProps } from 'react'
 import { NotePreview } from './NotePreview'
+import { twMerge } from 'tailwind-merge'
 
-export const NotePreviewList = ({ ...props }: ComponentProps<'ul'>) => {
+export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
+  if (notesMock.length === 0) {
+    return (
+      <ul className={twMerge('text-center pt-4', className)} {...props}>
+        <span>No Notes Yet!</span>
+      </ul>
+    )
+  }
+
   return (
-    <ul {...props}>
+    <ul className={className} {...props}>
       {notesMock.map((note, i) => (
         <NotePreview key={i} {...note} />
       ))}
