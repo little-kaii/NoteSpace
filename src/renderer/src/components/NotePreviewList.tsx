888b311214952bc1881a5011a38d1046fbbe4216
/Akash <akashdebnathwd@gmail.com>
2024-03-09 20:21:29 +0530

    Working on file system integration

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -3,6 +3,7 @@ import { ComponentProps } from 'react'
 import { NotePreview } from '@/components'
 import { twMerge } from 'tailwind-merge'
 import { useNotesList } from '@/hooks/useNotesList'
+import { isEmpty } from 'lodash'
 
 export type NotePreviewListProps = ComponentProps<'ul'> & {
   onSelect?: () => void
@@ -11,7 +12,9 @@ export type NotePreviewListProps = ComponentProps<'ul'> & {
 export const NotePreviewList = ({ className, onSelect, ...props }: NotePreviewListProps) => {
   const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({ onSelect })
 
-  if (notes.length === 0) {
+  if (!notes) return null
+
+  if (isEmpty(notes)) {
     return (
       <ul className={twMerge('text-center pt-4', className)} {...props}>
         <span>No Notes Yet!</span>
